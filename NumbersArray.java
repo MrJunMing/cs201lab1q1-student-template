30dd45606f1e0f595783ba1c27a5adb095022820
JM <arrak@Admin.>
2026-09-06 20:36:09 +0800

    submit

--- NumbersArray.java
+++ NumbersArray.java
@@ -19,7 +19,47 @@ public class NumbersArray {
         System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
     }
 
-    // Write your methods here
-    
+    public static Integer findMax(Integer[] input) {
+        Integer max = input[0];
+
+        for (Integer num : input) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+
+    public static Integer[] findDuplicates(Integer[] input) {
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new LinkedHashSet<>();
+
+        for (Integer num : input) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        return duplicates.toArray(new Integer[0]);
+    }
+
+    public static Integer[] findUnique(Integer[] input) {
+        Map<Integer, Integer> count = new LinkedHashMap<>();
+
+        for (Integer num : input) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> unique = new ArrayList<>();
+
+        for (Integer num : input) {
+            if (count.get(num) == 1) {
+                unique.add(num);
+            }
+        }
+
+        return unique.toArray(new Integer[0]);
+    }
 }
 
